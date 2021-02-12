6f16908d474afdc46f936acaa37a5df9fc25b1f2
notaparrot <hugo.saksik@gmail.com>
2021-02-12 11:11:16 +0100

    add hyphenation script

---
+++ hyphens.js
@@ -0,0 +1,26 @@
+var paged = new Paged.Previewer();
+var Hyphenopoly = {
+    require: {
+        "fr": "anticonstitutionnellement",
+    },
+    setup: {
+        exceptions: {
+            "global": "Bernard, Stiegler,",
+        },
+        selectors: {
+            "*":{
+                minWordlength: 7,
+                orphanControl: 2
+            }
+        },
+        paths: {
+            patterndir: "Hyphenopoly/patterns/",
+            maindir: "Hyphenopoly/"
+        },
+        handleEvent: {
+            hyphenopolyEnd: function (e) {
+                paged.preview();
+            }
+        }
+    }
+};
